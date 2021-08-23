b3e098f93e5abc63de5ccfa9fd8d03b3550c901b
Simon Cropp <simon.cropp@gmail.com>
2021-08-23 12:07:28 +1000

    .

--- src/NullabilityInfo/SR.cs
+++ src/NullabilityInfo/SR.cs
@@ -1,7 +1,41 @@
-﻿namespace System.Reflection
+﻿using System;
+using System.Reflection;
+
+class SR
 {
-    internal class SR
+    public static string NullabilityInfoContext_NotSupported = "NullabilityInfoContext is not supported";
+}
+
+static class TypeExtensions
+{
+    public static MemberInfo GetMemberWithSameMetadataDefinitionAs(this Type type, MemberInfo member)
     {
-        public static string NullabilityInfoContext_NotSupported = "NullabilityInfoContext is not supported";
+        if (member is null) throw new ArgumentNullException(nameof(member));
+
+        const BindingFlags all = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance;
+        foreach (MemberInfo myMemberInfo in type.GetMembers(all))
+        {
+            if (myMemberInfo.HasSameMetadataDefinitionAs(member))
+            {
+                return myMemberInfo;
+            }
+        }
+
+        throw new($"Member not found:{member.Name}");
+    }
+
+    //https://github.com/dotnet/runtime/blob/main/src/coreclr/System.Private.CoreLib/src/System/Reflection/MemberInfo.Internal.cs
+    static bool HasSameMetadataDefinitionAs(this MemberInfo target, MemberInfo other)
+    {
+        if (other is null)
+            throw new ArgumentNullException(nameof(other));
+
+        if (target.MetadataToken != other.MetadataToken)
+            return false;
+
+        if (!target.Module.Equals(other.Module))
+            return false;
+
+        return true;
     }
 }
